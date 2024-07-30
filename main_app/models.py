895d0edf7431bbde09ce91303a9ec4adc1d3131e
Clint Eastman <clint@Clints-MacBook-Air.local>
2024-07-30 13:09:06 -0400

    added models for users, trips and itinerary

--- main_app/models.py
+++ main_app/models.py
@@ -2,5 +2,29 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class UserProfile(models.Model):
+    name = models.CharField(max_length=255)
+    interests = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+
+class Trip(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="trips")
+    itinerary = models.ForeignKey(
+        "Itinerary", on_delete=models.CASCADE, related_name="trips"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Itinerary(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name} - {self.location}"
