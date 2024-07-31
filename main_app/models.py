867bef9ff49aa96e305ee837cc3dd16504752ca3
Clint Eastman <clint@Clints-MacBook-Air.local>
2024-07-31 09:25:58 -0400

    connected user to userprofile

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
 
 
 class UserProfile(models.Model):
     name = models.CharField(max_length=255)
-    interests = models.TextField()
+    interests = models.TextField(default="")
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
 
     def __str__(self):
