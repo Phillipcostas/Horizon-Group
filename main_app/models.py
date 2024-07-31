adefef2a7a0523083965b533973aaf16a4c95f27
Clint Eastman <clint@Clints-MacBook-Air.local>
2024-07-30 21:12:27 -0400

    updated model to the version we wanted.

--- main_app/models.py
+++ main_app/models.py
@@ -17,9 +17,6 @@ class Trip(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="trips")
     # start_date = models.DateField()
     # end_date = models.DateField()
-    itinerary = models.ForeignKey(
-        "Itinerary", on_delete=models.CASCADE, related_name="trips"
-    )
 
     def __str__(self):
         return self.name
@@ -28,6 +25,7 @@ class Trip(models.Model):
 class Itinerary(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name="itineraries")
 
     def __str__(self):
         return f"{self.name} - {self.location}"
