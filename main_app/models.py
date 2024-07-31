e824bdab8aef775595833b0f9ce468a6fec1b17b
josephwjcho93@gmail.com <josephcho@Josephs-MacBook-Pro.local>
2024-07-31 11:13:52 -0400

    added the formfield for ADD TRIP

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.exceptions import ValidationError
+from datetime import date
 
 class UserProfile(models.Model):
     name = models.CharField(max_length=255)
@@ -14,11 +15,22 @@ class UserProfile(models.Model):
 class Trip(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="trips")
-    # start_date = models.DateField()
-    # end_date = models.DateField()
+    location = models.CharField(max_length=255, default="Unknown Location")
+    start_date = models.DateField(default=date.today)
+    end_date = models.DateField(default=date.today)
 
     def __str__(self):
         return self.name
+
+    def clean(self):
+        if self.start_date and self.end_date:
+            if self.end_date < self.start_date:
+                raise ValidationError('End date cannot be before start date')
+        else:
+            if not self.start_date:
+                raise ValidationError('Start date is required')
+            if not self.end_date:
+                raise ValidationError('End date is required')
 
 
 class Itinerary(models.Model):
