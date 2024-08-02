cb43071aa6b754b4040b70c59ac1b4a4cd4c8b77
josephwjcho93@gmail.com <josephcho@Josephs-MacBook-Pro.local>
2024-08-02 15:07:07 -0400

    it works

--- create-database.sql
+++ create-database.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS horizon;
 CREATE USER horizon_admin WITH PASSWORD 'password';
 
-CREATE DATABASE horizon with OWNER=horizon_admin;
+CREATE DATABASE horizon with OWNER "horizon_admin";
 
 
