ce26f27e6ec545d729b8727bcb7aed4a435e675c
Clint Eastman <clint@Clints-MacBook-Air.local>
2024-08-05 12:05:05 -0400

    added photo models rework and profile data

--- main_app/views.py
+++ main_app/views.py
@@ -7,10 +7,9 @@ from .models import UserProfile, Trip, Itinerary, SuitcaseItem, UserPhoto, TripP
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
-from .froms import TripForm, SuitcaseItemForm
+from .froms import TripForm, SuitcaseItemForm, ProfilePhotoForm
 from datetime import date, timedelta
 from collections import defaultdict
-
 
 
 def about(request):
@@ -24,7 +23,8 @@ def map(request):
 
 def trip_index(request):
     trips = Trip.objects.filter(user=request.user)
-    return render(request, 'trip.html', { 'trips': trips })
+    return render(request, "trip.html", {"trips": trips})
+
 
 def signup(request):
     error_message = ""
@@ -43,9 +43,10 @@ def signup(request):
     context = {"form": form, "error_message": error_message}
     return render(request, "signup.html", context)
 
+
 @login_required
 def suitcase_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = SuitcaseItemForm(request.POST)
         if form.is_valid():
             item = form.save(commit=False)
@@ -53,58 +54,71 @@ def suitcase_view(request):
             item.save()
             # checkbox_value = form.cleaned_data['my_checkbox']
 
-            return redirect('suitcase')
+            return redirect("suitcase")
     else:
         form = SuitcaseItemForm()
 
     items = SuitcaseItem.objects.filter(user=request.user)
-    categories = ['Essentials', 'Toiletries', 'Speciality Clothes', 'Lounge Wear']
-    categorized_items = {category: items.filter(category=category) for category in categories}
-    context = {'form': form, 'categorized_items': categorized_items}
-    return render(request, 'suitcase.html', context)
+    categories = ["Essentials", "Toiletries", "Speciality Clothes", "Lounge Wear"]
+    categorized_items = {
+        category: items.filter(category=category) for category in categories
+    }
+    context = {"form": form, "categorized_items": categorized_items}
+    return render(request, "suitcase.html", context)
+
 
 @login_required
 def remove_suitcase_item(request, pk):
     item = get_object_or_404(SuitcaseItem, pk=pk, user=request.user)
     item.delete()
-    return redirect('suitcase')
+    return redirect("suitcase")
+
 
 @login_required
 def toggle_packed_status(request, pk):
     item = get_object_or_404(SuitcaseItem, pk=pk, user=request.user)
-    if 'packed' in request.POST:
+    if "packed" in request.POST:
         item.packed = not item.packed
-    if 'quantity' in request.POST:
-        item.quantity = request.POST.get('quantity')
+    if "quantity" in request.POST:
+        item.quantity = request.POST.get("quantity")
     item.save()
-    return redirect('suitcase')
+    return redirect("suitcase")
 
 
 class Home(LoginRequiredMixin, LoginView):
     def get(self, request):
         upcoming_trips, other_trips = self.get_trips(request.user)
-        return render(request, 'home.html', {
-            'upcoming_trips': upcoming_trips,
-            'other_trips': other_trips,
-                                             
-        })
-    
+        return render(
+            request,
+            "home.html",
+            {
+                "upcoming_trips": upcoming_trips,
+                "other_trips": other_trips,
+            },
+        )
+
     def get_trips(self, user):
         today = date.today()
         ten_days_later = today + timedelta(days=10)
-        upcoming_trips = Trip.objects.filter(user=user, start_date__range=(today, ten_days_later))
-        other_trips = Trip.objects.filter(user=user).exclude(start_date__range=(today, ten_days_later))
+        upcoming_trips = Trip.objects.filter(
+            user=user, start_date__range=(today, ten_days_later)
+        )
+        other_trips = Trip.objects.filter(user=user).exclude(
+            start_date__range=(today, ten_days_later)
+        )
         return upcoming_trips, other_trips
 
 
 class Login(LoginView):
-    template_name = 'login.html'
+    template_name = "login.html"
+
 
 class AddTrip(LoginRequiredMixin, View):
     trip_photos = TripPhoto.objects.all
+
     def get(self, request):
         form = TripForm()
-        return render(request, 'addTrip.html', {'form': form})
+        return render(request, "addTrip.html", {"form": form})
 
     def post(self, request):
         form = TripForm(request.POST)
@@ -112,38 +126,65 @@ class AddTrip(LoginRequiredMixin, View):
             trip = form.save(commit=False)
             trip.user = request.user
             trip.save()
-            return redirect('trip_list')  
-        return render(request, 'addTrip.html', {'form': form})
-    
+            return redirect("trip_list")
+        return render(request, "addTrip.html", {"form": form})
+
+
 class TripListView(LoginRequiredMixin, View):
     def get(self, request):
         trips = Trip.objects.filter(user=request.user)
-        return render(request, 'trip.html', {'trips': trips})
-    
+        return render(request, "trip.html", {"trips": trips})
+
+
 class TripDetailView(LoginRequiredMixin, View):
     def get(self, request, pk):
         trip = get_object_or_404(Trip, pk=pk, user=request.user)
         trip_photo_id = trip.trip_photo_id
-        trip_photo = TripPhoto.objects.filter(id = trip_photo_id)
+        trip_photo = TripPhoto.objects.filter(id=trip_photo_id)
         photo = trip_photo[0]
         num_days = trip.number_of_days()
         itineraries_by_day = {}
         for day in range(1, num_days + 1):
             itineraries_by_day[day] = list(Itinerary.objects.filter(trip=trip, day=day))
-        return render(request, 'trip_detail.html', {'trip': trip, 'num_days': num_days, 'itineraries_by_day': itineraries_by_day, 'photo': photo})
+        return render(
+            request,
+            "trip_detail.html",
+            {
+                "trip": trip,
+                "num_days": num_days,
+                "itineraries_by_day": itineraries_by_day,
+                "photo": photo,
+            },
+        )
 
 
 class AddItinerary(LoginRequiredMixin, View):
     def post(self, request, pk, day):
         trip = get_object_or_404(Trip, pk=pk, user=request.user)
-        itinerary_name = request.POST.get('itinerary_name')
+        itinerary_name = request.POST.get("itinerary_name")
         if itinerary_name:
             Itinerary.objects.create(trip=trip, day=day, name=itinerary_name)
             print(f"Added itinerary: {itinerary_name} to day: {day}")  # Debugging line
         else:
             print("Itinerary name is empty")  # Debugging line
-        return redirect('trip_detail', pk=pk)
+        return redirect("trip_detail", pk=pk)
+
 
 def profile(request):
     userProfile = UserProfile.objects.get(user=request.user)
-    return render(request, 'profile.html', {'userProfile': userProfile})
+
+    if request.method == "POST":
+        form = ProfilePhotoForm(request.POST)
+        if form.is_valid():
+            selected_photo = form.cleaned_data["profile_photo"]
+            userProfile.profile_photo = selected_photo
+            userProfile.save()
+            return redirect("profile")
+    else:
+        form = ProfilePhotoForm()
+
+    context = {
+        "userProfile": userProfile,
+        "form": form,
+    }
+    return render(request, "profile.html", context)
